761b70f1e2885f9907f6b925f8f863b29f9fe725
Marius Baron <marius.baron@gmail.com>
2016-08-21 18:10:24 +0200

    add awesome back to home plugin

--- www/js/config.js
+++ www/js/config.js
@@ -1,40 +1,45 @@
 
-(function () {
-    'use strict';
+angular.module('Open')
+  .run(($ionicPlatform, $state, FriendsFactory, SettingsFactory, AmplitudeFactory) => {
 
-    angular.module('Open')
-      .run(function($ionicPlatform, FriendsFactory, SettingsFactory, AmplitudeFactory) {
- 
-      $ionicPlatform.ready(function() {
-        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-        // for form inputs)
-        if (window.cordova && window.cordova.plugins.Keyboard) {
-          cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-          cordova.plugins.Keyboard.disableScroll(true);
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
 
-        }
-        if (window.StatusBar) {
-          // org.apache.cordova.statusbar required
-          StatusBar.styleDefault();
-        }
-      });
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
 
-      SettingsFactory.init();
+    $ionicPlatform.registerBackButtonAction(function(e){
+      if($state.current.name == 'friends') {
+        backAsHome.trigger();
+        e.preventDefault();
+      } else {
+        navigator.app.backHistory();
+      }
+    },101);     
+  });
 
-      AmplitudeFactory.init();
+  SettingsFactory.init();
 
-      FriendsFactory.load();
-    })
+  AmplitudeFactory.init();
 
-    .constant('appConfig', {
-      // app version number
-      'version': '0.6.1',
+  FriendsFactory.load();
+})
 
-      // api key for tracking
-      'amplitudeApiKey': '868bce7777d5c7fef2e1cd586a0dc33d'
-    })
-    
-    // lodash
-    .constant('_', window._);
+.constant('appConfig', {
+  // app version number
+  'version': '0.6.1',
 
-})();
+  // api key for tracking
+  'amplitudeApiKey': '868bce7777d5c7fef2e1cd586a0dc33d'
+})
+
+// lodash
+.constant('_', window._);
+
